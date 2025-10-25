b848c4b0955d8f00a0f0ebb31234a8be287a7c4e
MyslZhao <vselennaya@163.com>
2025-10-25 09:15:29 +0800

    finished

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,15 +16,28 @@ st.markdown(
     "_通过科学算法预测父母性别_"
     )
 
+
 frame = st.empty()
-
 with frame.container():
-    
+    ques_1 = st.radio(f"你吃过{QUES[0]}了吗？",["是","否"],index=None,key=1)
 
-st.markdown("""
-你的父亲是：  
-## 男性
-你的母亲是：  
-## 女性
-""")
+if ques_1 in ["是","否"]:
+    frame.empty()
+    with frame.container():
+        ques_1 = st.radio(f"你吃过{QUES[1]}了吗？",["是","否"],index=None,key=2)
+
+if ques_1 in ["是","否"]:
+    frame.empty()
+    with frame.container():
+        ques_1 = st.radio(f"你吃过{QUES[2]}了吗？",["是","否"],index=None,key=3)
+    
+if ques_1 in ["是","否"] :
+    frame.empty()
+    with frame.container():
+        st.markdown("""
+        你的父亲是：  
+        ## 男性
+        你的母亲是：  
+        ## 女性
+        """)
 
